395d09326bfd1c83c54ae3ea505d5ae1e1547f32
ashutosh-080797 <ashutosh080797@gmail.com>
2025-05-21 00:40:29 +0530

    fix restart

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -6,8 +6,8 @@ pkill -f serve || true
 
 # Start backend (Python)
 cd /home/ec2-user/jobify/backend
-pip install -r requirements.txt
-nohup uvicorn main:app --host 0.0.0.0 --port 8000 &
+/usr/bin/pip3.11 install --user -r requirements.txt
+/usr/local/bin/uvicorn main:app --host 0.0.0.0 --port 8000 &
 
 # Start frontend (React)
 cd /home/ec2-user/jobify/frontend-app
